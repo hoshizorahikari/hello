2e2a6c0dc7b4fbf144aa34f54606a8a71aa0d6a8
hoshizorahikari <208343741@qq.com>
2018-05-03 10:45:17 +0800

    表单

--- hello.py
+++ hello.py
@@ -1,18 +1,42 @@
-from flask import Flask, render_template
+from flask import Flask, redirect, render_template, session, url_for, flash
 from flask_bootstrap import Bootstrap
-from flask_moment import Moment
-from datetime import datetime
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired
+
+# from flask_moment import Moment
+# from datetime import datetime
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'hoshizora rin'
+
+
 bootstrap = Bootstrap(app)
-moment = Moment(app)
+# moment = Moment(app)
 
 # ----------------
 
 
-@app.route('/')
+class NameForm(FlaskForm):  # 一个文本字段和一个提交按钮
+    name = StringField('你的名字是？', validators=[DataRequired()])
+    submit = SubmitField('提交')
+
+
+@app.route('/', methods=['GET', 'POST'])  # 支持GET和POST
 def index():
-    return render_template('index.html', now=datetime.utcnow())
+    form = NameForm()
+    if form.validate_on_submit():  # 提交表单,数据被验证函数接受
+        old_name = session.get('name')  # 获取session已有name
+        if old_name and old_name != form.name.data:
+            flash('你似乎改名字了！')
+        session['name'] = form.name.data  # 获取字段data属性存入session
+        return redirect(url_for('index'))  # 重定向,GET方式请求index
+    return render_template('index.html', form=form, name=session.get('name'))
+
+
+# @app.route('/')
+# def index():
+#     return render_template('index.html', now=datetime.utcnow())
 
 
 @app.route('/user/<name>')
